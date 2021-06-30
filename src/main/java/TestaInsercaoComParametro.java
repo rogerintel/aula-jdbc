061bf2cf5c65a4aaefcac4d37a9fa9c3732d2ebf
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-06-29 22:41:16 -0300

    Aula 4

--- src/main/java/TestaInsercaoComParametro.java
+++ src/main/java/TestaInsercaoComParametro.java
@@ -2,20 +2,30 @@ import java.sql.*;
 
 public class TestaInsercaoComParametro {
 
-    public static void main(String[] args) throws SQLException {
+    public static void main(String[] args) {
 
-        String nome = "Mouse";
-        String descricao = "Mouse sem fio";
-        Connection connection = new ConnectionFactory().getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement("insert into produto (nome, descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS);
+        try (Connection connection = new ConnectionFactory().getConnection()) {
+            connection.setAutoCommit(false);
+            try (PreparedStatement preparedStatement = connection.prepareStatement("insert into produto (nome, " +
+                    "descricao) VALUES (?,?)", Statement.RETURN_GENERATED_KEYS)) {
+                adicionarProduto("SmartTV", "45 polegadas", preparedStatement);
+                adicionarProduto("Rádio", "Rádio a bateria", preparedStatement);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
+    private static void adicionarProduto(String nome, String descricao, PreparedStatement preparedStatement)
+            throws SQLException {
         preparedStatement.setString(1, nome);
         preparedStatement.setString(2, descricao);
         preparedStatement.execute();
-        ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
-        while (generatedKeys.next()){
-            int id = generatedKeys.getInt(1);
-            System.out.println("O id criado foi: " + id);
+        try (ResultSet generatedKeys = preparedStatement.getGeneratedKeys()) {
+            while (generatedKeys.next()) {
+                int id = generatedKeys.getInt(1);
+                System.out.println("O id criado foi: " + id);
+            }
         }
-        connection.close();
     }
 }
