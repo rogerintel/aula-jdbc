072257f0390e039f952c8651d7333ed15f0b6176
Rogério Carvalho <rogerio.carvalho@unifesspa.edu.br>
2021-06-29 23:06:01 -0300

    Aula 5

--- src/main/java/ConnectionFactory.java
+++ src/main/java/ConnectionFactory.java
@@ -1,9 +1,21 @@
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
+import javax.sql.DataSource;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ConnectionFactory {
+    private DataSource dataSource;
+
+    public ConnectionFactory() {
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost/loja_virtual");
+        comboPooledDataSource.setUser("root");
+        comboPooledDataSource.setPassword("teste");
+        this.dataSource = comboPooledDataSource;
+    }
+
     public Connection getConnection() throws SQLException {
-        return DriverManager.getConnection("jdbc:mysql://localhost/loja_virtual", "root", "teste");
+        return this.dataSource.getConnection();
     }
 }
